13726405286c00cdc010b394aa1e5adbeda11de5
lilei27 <lilei27@baidu.com>
2015-11-30 10:03:07 +0800

    test

--- redux/todomvc/store/configureStore.js
+++ redux/todomvc/store/configureStore.js
@@ -1,19 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 
-function logger({ getState }) {
-    return (next) => (action) => {
-        console.log('will dispatch', action);
+function logger({ getState }){
+    return function (next) {
+        return function (action) {
+            console.log('will dispatch', action);
 
-        // 调用 middleware 链中下一个 middleware 的 dispatch。
-        let returnValue = next(action);
+            // 调用 middleware 链中下一个 middleware 的 dispatch。
+            let returnValue = next(action);
 
-        console.log('state after dispatch', getState());
+            console.log('state after dispatch', getState());
 
-        // 一般会是 action 本身，除非
-        // 后面的 middleware 修改了它。
-        return returnValue;
-    };
+            // 一般会是 action 本身，除非
+            // 后面的 middleware 修改了它。
+            return returnValue;
+        }
+    }
 }
 
 let createStoreWithMiddleware = applyMiddleware(logger)(createStore);
