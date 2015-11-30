0761c52bca59e39ddaa6ddfc17b4e9bb04536e63
lilei27 <lilei27@baidu.com>
2015-11-30 10:55:12 +0800

    test

--- redux/todomvc/store/configureStore.js
+++ redux/todomvc/store/configureStore.js
@@ -1,5 +1,24 @@
-import { createStore } from 'redux'
-import rootReducer from '../reducers'
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers';
+
+function logger({ getState }){
+    return function (next) {
+        return function (action) {
+            console.log('will dispatch', action);
+
+            // 调用 middleware 链中下一个 middleware 的 dispatch。
+            let returnValue = next(action);
+
+            console.log('state after dispatch', getState());
+
+            // 一般会是 action 本身，除非
+            // 后面的 middleware 修改了它。
+            return returnValue;
+        }
+    }
+}
+
+let createStoreWithMiddleware = applyMiddleware(logger)(createStore);
 
 // 什么是reducers
 // 对照flux对应的demo,也就是todoStores中,
@@ -7,8 +26,7 @@ import rootReducer from '../reducers'
 // reducer实际就是register函数内部的部分;随着业务的增加,这部分内容势必增加;
 export default function configureStore(initialState) {
     //initialState为初始化值;
-    const store = createStore(rootReducer, initialState);
-
+    const store = createStoreWithMiddleware(rootReducer, initialState);
     //也就是创建flux中的todoStore;
 
     if (module.hot) {
