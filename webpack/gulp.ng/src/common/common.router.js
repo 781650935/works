fb3bafe5851a432ff0e79ddd1ae0ad01ee82999f
lilei27 <lilei27@baidu.com>
2015-12-14 18:15:15 +0800

    test

---
+++ webpack/gulp.ng/src/common/common.router.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Rayr Lee on 2015/12/14.
+ */
+
+export default ($stateProvider, $urlRouterProvider) => {
+
+    'ngInject';
+    $urlRouterProvider.otherwise('/index');
+
+    // 常见问题&帮助界面
+};
